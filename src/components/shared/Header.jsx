4a51e5b7877b14211f44c40f5caacc93b08510fb
Matthew7991 <Matthew7991@gmx.de>
2023-09-15 15:26:35 +0200

    added link to admin panel

--- src/components/shared/Header.jsx
+++ src/components/shared/Header.jsx
@@ -1,9 +1,25 @@
 import React from "react"
+import { Link, useLocation } from "react-router-dom"
 
 function Header({ text }) {
+  const location = useLocation().pathname
+
   return (
-    <section className="bg-[#ffe81f] py-4 px-2 border-gray-500 border">
+    <section className="bg-[#ffe81f] py-4 px-2 border-gray-500 border flex justify-between items-baseline gap-4">
       <h1 className="text-2xl">{text}</h1>
+      {location === "/" ? (
+        <Link
+          className="text-blue-400 underline"
+          to={"/dashboard"}>
+          Admin
+        </Link>
+      ) : (
+        <Link
+          className="text-blue-400 underline"
+          to={"/"}>
+          Home
+        </Link>
+      )}
     </section>
   )
 }
